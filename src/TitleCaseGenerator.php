5bb22e0c661b2f573aad35e34590df0b2d196ad5
Alex Costa & Jeff Austin <student@epicodus.com>
2015-08-10 12:00:04 -0700

    Lowercase preps and cons

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -3,7 +3,41 @@ class TitleCaseGenerator
 {
     function makeTitleCase($input_title)
     {
-        
+        //Preposition and conjunction array
+        $prep_con = array("a","an","and","as","at","but","by",
+        "en","for","if","in","of","on","or","the","to", "from");
+
+        //Seperates string by space and stores into an array
+        $input_array_of_words = explode(" ", $input_title);
+
+        //Storage array
+        $output_titlecased = array();
+
+        //Caps the first letter of all words
+        foreach($input_array_of_words as $word) {
+
+
+            //Checks to see if the input is a conjuction or prep
+            /// if it is then we make sure to not capitalize it.
+            if(in_array($word, $prep_con)){
+
+                array_push($output_titlecased, $word);
+
+            }else{
+
+                array_push($output_titlecased, ucfirst($word));
+                
+            }
+
+        }
+
+
+
+        return implode(" ", $output_titlecased);
+
+
     }
+
 }
+
 ?>
--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -5,7 +5,7 @@
     class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
     {
 
-            function test_makeTitleCase_oneWord()
+            function test_makeTitleCase_oneLetter()
             {
                 // Arrange
                 $test_TitleCaseGenerator = new TitleCaseGenerator;
@@ -17,6 +17,46 @@
                 //Assert
                 $this->assertEquals("I", $result);
             }
+
+            function test_makeTitleCase_oneWord()
+            {
+                //Arrange
+                $test_TitleCaseGenerator = new TitleCaseGenerator;
+                $input = "beowulf";
+
+                //Act
+                $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+                //Assert
+                $this->assertEquals("Beowulf", $result);
+            }
+
+            function test_makeTitleCase_multipleWords()
+            {
+                // Arrange
+                $test_TitleCaseGenerator = new TitleCaseGenerator;
+                $input = "beowulf begins";
+
+                // Act
+                $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+                // Assert
+                $this->assertEquals("Beowulf Begins", $result);
+            }
+
+            function test_makeTitleCase_conjunctionIgnore()
+            {
+                // Arrange
+                $test_TitleCaseGenerator = new TitleCaseGenerator;
+                $input = "beowulf from brighton beach";
+
+                // Act
+                $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+                // Assert
+                $this->assertEquals("Beowulf from Brighton Beach", $result);
+            }
+
     }
 
 ?>
