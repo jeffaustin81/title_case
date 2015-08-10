b6bb659ee6b40d99dd90d187a20ef297df6ad554
Alex Costa & Jeff Austin <student@epicodus.com>
2015-08-10 10:31:00 -0700

    Add zero case test

---
+++ tests/TitleCaseGeneratorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once "src/TitleCaseGenerator.php";
+
+    class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
+    {
+
+            function test_makeTitleCase_oneWord()
+            {
+                // Arrange
+                $test_TitleCaseGenerator = new TitleCaseGenerator;
+                $input = "beowulf";
+
+                //act
+                $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+                //Assert
+                $this->assertEquals("Beowulf", $result);
+            }
+    }
+
+?>
